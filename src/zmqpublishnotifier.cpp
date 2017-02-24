4d11b6190808770eb71e4bb957658701c77a8a1c
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2017-02-24 03:59:27 -0600

    Add bitcoin common crypto functions

--- src/zmqpublishnotifier.cpp
+++ src/zmqpublishnotifier.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 //#include "chainparams.h"
+#include "common.h"
 //#include "streams.h"
 #include "zmqpublishnotifier.h"
 //#include "validation.h"
